e57bbf4a16a6d6507f4f0a7a050e3eb4436b267e
Jaroslav Kafka <jaroslav.kafka@elso-systems.com>
2022-12-02 11:31:22 +0100

    refaktoring

--- 2_day/2_day_H.py
+++ 2_day/2_day_H.py
@@ -1,20 +1,15 @@
 file1 = open('.\\2_day\\text.txt', 'r')
 Lines = file1.readlines()
 
-strategy = { "A": 1, "X": 0, "B": 2, "Y": 1, "C": 3, "Z": 2 }
+strategy = { "A": 0, "X": 0, "B": 1, "Y": 1, "C": 2, "Z": 2 }
 count = 0
-points = [1, 2, 3]
 
 for line in Lines:
-    elf, me = line.replace("\n", "").split(" ")
-    elf = strategy[elf] -1
-    me = strategy[me]
+    elf, resault = line.replace("\n", "").split(" ")
+    elf = strategy[elf] -1 #-1 protože změna logiky
+    resault = strategy[resault]  
+
+    count += resault * 3 #lineární průběh => X == prohra, Y == remíza a Z == výhra
+    count +=  ((resault + elf) % 3) + 1  #
     
-    if elf == me:
-        count += 3      
-    elif (me - elf) % 3 == 1:
-        count += 6   
-    
-    count += me + 1
-            
 print(count)
