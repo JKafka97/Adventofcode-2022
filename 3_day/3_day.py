91bcc3a6f270ea38ec9f116f09d269898dfbac87
Jaroslav Kafka <jaroslav.kafka@elso-systems.com>
2022-12-05 09:28:41 +0100

    ez day 3 done

---
+++ 3_day/3_day.py
@@ -0,0 +1,19 @@
+import string
+
+file1 = open('.\\3_day\\text.txt', 'r')
+Lines = file1.readlines()
+
+my_dict = {}
+score = 0
+alphabet = string.ascii_lowercase + string.ascii_uppercase
+
+for count, value in enumerate(alphabet):
+    my_dict[value] = count+1
+
+for line in Lines:
+    lenght = len(line)//2
+    match = set(line[:lenght]).intersection(line[lenght:])
+    score +=  my_dict[match.pop()]
+
+print(score)
+    
