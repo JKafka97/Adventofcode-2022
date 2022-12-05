83eacc64ebc00bb24fe52e5ae2af094c51151551
Jaroslav Kafka <jaroslav.kafka@elso-systems.com>
2022-12-05 10:02:23 +0100

    day 3 hard version done

---
+++ 3_day/3_day_H.py
@@ -0,0 +1,23 @@
+import string
+
+file1 = open('.\\3_day\\text.txt', 'r')
+Lines = file1.readlines()
+
+my_dict = {}
+my_list = []
+score = 0
+alphabet = string.ascii_lowercase + string.ascii_uppercase
+
+for count, value in enumerate(alphabet):
+    my_dict[value] = count+1
+
+for line in Lines:
+    my_list.append(line.replace("\n", ""))
+    if len(my_list) < 3:
+        continue
+    match = set(my_list[0]).intersection(set(my_list[1]), set(my_list[2]))
+    score +=  my_dict[match.pop()]
+    my_list.clear()
+
+print(score)
+    
